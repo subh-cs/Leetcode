8d00ff740b5da0c96281733614e5b89243407656
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-06-14 08:22:16 +0530

    Time: 1550 ms (5.02%), Space: 420.3 MB (5.02%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,14 +1,13 @@
-int dp[501][501];
-int f(int i,int j, string a, string b){
+int f(int i,int j, string a, string b,map<pair<int,int>,int>&m){
     if(i==a.length()||j==b.length()) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(a[i]==b[j]) return dp[i][j]=1+f(i+1,j+1,a,b);
-    return dp[i][j]= max(f(i+1,j,a,b),f(i,j+1,a,b));
+    if(m.find({i,j})!=m.end()) return m[{i,j}];
+    if(a[i]==b[j]) return m[{i,j}]=1+f(i+1,j+1,a,b,m);
+    return m[{i,j}]= max(f(i+1,j,a,b,m),f(i,j+1,a,b,m));
 }
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof dp);
-        return word1.length()+word2.length()-(2*f(0,0,word1,word2));
+        map<pair<int,int>,int>m;
+        return word1.length()+word2.length()-(2*f(0,0,word1,word2,m));
     }
 };
