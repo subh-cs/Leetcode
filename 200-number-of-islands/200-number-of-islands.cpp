22fbeb8b04ee0ec16e8488d30752ced6556e523a
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-07-05 09:16:15 +0530

    Time: 96 ms (10.16%), Space: 20 MB (8.31%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -6,6 +6,20 @@ void dfs(int i,int j, int m, int n, vector<vector<char>>&grid){
         dfs(i+1,j,m,n,grid);
         dfs(i-1,j,m,n,grid);
 }
+void bfs(int i,int j, int m, int n, vector<vector<char>> &grid){
+    queue<pair<int,int>>q;
+    q.push({i,j});
+    while(!q.empty()){
+        auto node = q.front();q.pop();
+        i=node.first,j=node.second;
+        if(i<0 || j<0 || i==m || j==n || grid[i][j]=='0') continue;
+        grid[i][j]='0';
+        q.push({i,j-1});
+        q.push({i,j+1});
+        q.push({i+1,j});
+        q.push({i-1,j});
+    }
+}
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
@@ -13,7 +27,7 @@ public:
         for(int i=0;i<m;++i)
             for(int j=0;j<n;++j)
                 if(grid[i][j]=='1'){
-                    dfs(i,j,m,n,grid);
+                    bfs(i,j,m,n,grid);
                     count++;
                 }
         return count;
