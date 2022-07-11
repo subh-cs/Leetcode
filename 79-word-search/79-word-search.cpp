b19714fdbd7c2e493fa300f70f2fc611c0b80320
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-07-11 22:18:43 +0530

    Time: 1388 ms (5.16%), Space: 7.9 MB (73.77%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    //pass board by reference
-    bool DFS(vector<vector<char>>& board, string word, int i, int j, int n) {
-		//check if all the alphabets in the word is checked
-        if(n == word.size()) return true; 
+    bool found(vector<vector<char>>& board, string word, int r, int c, int idx){
+        if(idx==word.size()) return true;
+        if(r<0 || r==board.size() || c<0 || c==board[0].size() || board[r][c]=='0' || word[idx] != board[r][c]) return false;
         
-		//check if i and j are out of bound or if the characters aren't equal
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) return false;
+        char ch = board[r][c];
+        board[r][c] = '0';
         
-		//mark as visited 
-        board[i][j] = '0';
+        bool bottom = found(board,word,r+1,c,idx+1);
+        bool top = found(board,word,r-1,c,idx+1);
+        bool right = found(board,word,r,c+1,idx+1);
+        bool left = found(board,word,r,c-1,idx+1);
         
-		//branch out in all 4 directions
-        bool status = DFS(board, word, i + 1, j, n + 1) ||  //down
-                        DFS(board, word, i, j + 1, n + 1) ||  //right
-                        DFS(board, word, i - 1, j, n + 1) ||  //up
-                        DFS(board, word, i, j - 1, n + 1);  //left
-        
-		//change the character back for other searches
-        board[i][j] = word[n];
-		
-        return status;
+        board[r][c] = ch;
+        return top || bottom || left || right;
     }
-    
     bool exist(vector<vector<char>>& board, string word) {
-        if(word == "") return false;
+        int m = board.size();
+        int n = board[0].size();
         
-        for(int i = 0; i < board.size(); i++) 
-            for(int j = 0; j < board[i].size(); j++) 
-				//check if the characters are equal then call DFS
-                if(board[i][j] == word[0] && DFS(board, word, i, j, 0))
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(found(board,word,i,j,0)){
                     return true;
-        
+                }
+            }
+        }
         return false;
     }
 };
