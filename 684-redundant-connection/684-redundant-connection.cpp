f289dc96cf4705202920474236306619fde4f44e
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-07-11 21:29:22 +0530

    Time: 16 ms (36.75%), Space: 9.1 MB (36.55%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -5,11 +5,14 @@
 //         parent[i] = i; 
 // }
 
+
+
+
+class Solution {
 int findParent(int node, vector<int> &parent){
     if(node==parent[node]) return node;
     return parent[node] = findParent(parent[node], parent);
 }
-
 void unionn(int a, int b, vector<int>& parent, vector<int>& rank){
     a = findParent(a, parent);
     b = findParent(b, parent);
@@ -22,23 +25,16 @@ void unionn(int a, int b, vector<int>& parent, vector<int>& rank){
         rank[a]++;
     }
 }
-
 bool isSame(int a, int b, vector<int>& parent){
     if(findParent(a, parent)==findParent(b, parent)) return true;
     return false;
 }
-
-
-class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> rank(n+1,0);
-        vector<int> parent(n+1);
+        vector<int> rank(n+1,0),parent(n+1),ans; 
         for(int i = 1;i<=n;i++)
             parent[i]=i;
-        // setParent(n);
-        vector<int> vis(n+1,0),ans;
         int first,last;
         for(auto it: edges){
             if(isSame(it[0],it[1],parent)){
