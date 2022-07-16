6ad85ad606a21a8b998363171d5313f8848a27e0
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-07-16 20:14:50 +0530

    Time: 24 ms (84.01%), Space: 13.9 MB (59.93%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,21 +1,31 @@
-bool detectCycle(int node,vector<int>&vis,vector<int>adj[]){
-    if(vis[node]==1) return false;
-    if(vis[node]==2) return true;
-    vis[node]=1;
-    for(auto it: adj[node])
-        if(!detectCycle(it,vis,adj)) return false;
-    vis[node]=2; return true;
+bool detectCycle(vector<int>indegree,vector<int>adj[]){
+    // push the values with indegree = 0;
+    queue<int>q;
+    for(int i=0;i<indegree.size();i++)
+        if(indegree[i]==0) q.push(i);
+    int count=0;
+    //start bfs
+    while(!q.empty()){
+        int top= q.front();q.pop();
+        for(auto it: adj[top]){
+            indegree[it]--;
+            if(!indegree[it]) q.push(it);
+        }
+        count++;
+    }
+    if(count!=indegree.size()) return false;
+    return true;
 }
 
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& p) {
         if(p.size()==0) return true;
-        vector<int>adj[n];vector<int>vis(n);
-        for(auto it: p)
-            adj[it[1]].push_back(it[0]);
-        for(auto it: p)
-            if(!detectCycle(it[0],vis,adj)) return false;
-        return true;
+        vector<int>adj[n];vector<int>indegree(n);
+        for(auto it: p){
+            adj[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
+        }
+    return detectCycle(indegree,adj);
     }
 };
