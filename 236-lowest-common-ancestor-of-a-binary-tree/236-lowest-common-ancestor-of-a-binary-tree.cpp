81d6d2aa03711d58829985204f46aff05e281b18
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-07-26 08:36:46 +0530

    Time: 20 ms (73.61%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,18 +10,12 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //If I've found the value or NULL
-        if(!root || root==p || root==q) return root;
-        //Recursive calls will go-on and try to find the nodes
-        TreeNode* l = lowestCommonAncestor(root->left,p,q);
-        TreeNode* r = lowestCommonAncestor(root->right,p,q);
-        //if We've found the left & Right node, return root;
-        if(l && r) return root;
-        //If left is containing our value
+        if(!root||root==p||root==q) return root;
+        auto l = lowestCommonAncestor(root->left,p,q);
+        auto r = lowestCommonAncestor(root->right,p,q);
+        if(l&&r) return root;
         if(l) return l;
-        //If right is containing our value
         if(r) return r;
-        //if both are null
-        return NULL;
+        return l;
     }
 };
