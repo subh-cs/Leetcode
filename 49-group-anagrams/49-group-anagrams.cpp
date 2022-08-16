5e7b87cab01b97680aea4c0f89ea07fc678f746d
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-08-16 07:39:03 +0530

    Time: 74 ms (32.65%), Space: 21.8 MB (22.97%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         //it'll help to store indexes with one sorted anagram of string
-        unordered_map<string,vector<int>>m; 
+        unordered_map<string,vector<string>>m; 
         //
         for(int i=0;i<strs.size();i++){
             string temp = strs[i];
             sort(begin(temp),end(temp));
-            m[temp].push_back(i);
+            m[temp].push_back(strs[i]);
         }
         // now take all the anagrams from map
         vector<vector<string>>ans;
         for(auto it:m){
             vector<string>temp;
             for(auto jt:it.second)
-                temp.push_back(strs[jt]);
+                temp.push_back(jt);
             ans.push_back(temp);
         }
         return ans;
