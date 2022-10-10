ce7e13ad3bdf19cf0efa81e227d56137f59f5810
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-10-09 20:09:19 -0700

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,34 +1,9 @@
-
 class Solution {
 public:
-    bool isPalindrome(string s){
-    int n = s.size();
-    int i=0,j=n-1; 
-    while(i<=j){
-        if(s[i]!=s[j]) return false;
-        i++;j--;
-    }
-    return true;
-}
-    char validChar(int ind, string s){
-        for(int i=0;i<26;i++){
-            char temp = s[ind];
-            s[ind] = 'a'+ i;
-            if(!isPalindrome(s)) return s[ind];
-            s[ind] = temp;
-        }
-        return '0';
-    }
     string breakPalindrome(string s) {
-        vector<string>v;
-        for(int i=0;i<s.size();i++){
-            char c = validChar(i,s);
-            char temp = s[i];
-            if(c!='0') {s[i]=c;v.push_back(s);}
-            s[i]=temp;
-        }
-        sort(begin(v),end(v));
-        if(v.size()) return v[0];
-        return "";
+        int n = s.size();
+        for(int i=0;i<n/2;i++) if(s[i]!='a'){s[i]='a';return s;}
+        s[n-1] = 'b';
+        return n==1?"":s;
     }
 };
