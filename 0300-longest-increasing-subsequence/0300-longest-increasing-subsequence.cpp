cf3d56322bb2ffe35e95187cd1cb461d28da20af
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-10-10 20:41:32 -0700

    Time: 15 ms (86.57%), Space: 10.5 MB (70.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(),cnt=1;
-        vector<int>temp;temp.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            if(temp.back()<nums[i]) {temp.push_back(nums[i]);cnt++;}
-            else{
-                int ind = lower_bound(begin(temp),end(temp),nums[i])-begin(temp);
-                temp[ind]=nums[i];
-            }
-        }
-        return cnt;
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>::iterator m = nums.begin(); 
+    for (int& val : nums) {
+        auto it = lower_bound(nums.begin(), m, val);
+        *it = val;
+        if (it == m)
+            m++;
+    }
+    
+    return m - nums.begin()>=3 ? 1:0;
     }
 };
