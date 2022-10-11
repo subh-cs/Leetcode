bea888619ce10073ae56a039e3f148341cbb7aa3
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-10-10 20:42:10 -0700

    Time: 171 ms (18.61%), Space: 61.6 MB (67.34%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>::iterator m = nums.begin(); 
-    for (int& val : nums) {
-        auto it = lower_bound(nums.begin(), m, val);
-        *it = val;
-        if (it == m)
-            m++;
+        int n = nums.size(),cnt=1;
+        vector<int>temp;temp.push_back(nums[0]);
+        for (int i=1;i<n;i++) {    
+        if(nums[i]>temp.back()) {temp.push_back(nums[i]);cnt++;}
+        else{
+            int ind = lower_bound(temp.begin()+0, temp.end(), nums[i])-temp.begin();
+            temp[ind] = nums[i];
+        }
     }
-    
-    return m - nums.begin()>=3 ? 1:0;
+    return cnt>=3 ? 1:0;
     }
 };
