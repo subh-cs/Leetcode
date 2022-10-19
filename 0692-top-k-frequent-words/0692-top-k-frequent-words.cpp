a707c90b5aeaf42f00156e7f98bcadddb47def70
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-10-19 08:39:06 +0530

    Time: 36 ms (15.60%), Space: 12.6 MB (76.98%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -3,17 +3,17 @@
 
 class myComparator {
   public:
-    bool operator() (const psi &p1, const psi &p2) {
+   bool operator() (const psi &p1, const psi &p2) {
       if(p1.first == p2.first) return p1.second < p2.second;
       
       return p1.first > p2.first;
-    }
-
+   }
 };  
 
 
 class Solution {
 public:
+     
     vector<string> topKFrequent(vector<string>& words, int k) {
         int n = words.size();
 
